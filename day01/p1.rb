3a19819373c399080b54acc3c6a4564ffa10c76c
David Chen <cgl.david@gmail.com>
2019-02-16 00:01:29 -0800

    Refactor day 01

--- day01/p1.rb
+++
@@ -1,3 +0,0 @@
-File.open(File.join(__dir__, 'input.txt')) do |file|
-  puts file.each.map(&:to_i).sum
-end
---
+++ day01/solution.rb
@@ -0,0 +1,16 @@
+require 'set'
+
+def first_duplicate_frequency(changes)
+  frequency = 0
+  seen = Set.new
+  changes.cycle do |change|
+    frequency += change
+    return frequency if seen.add?(frequency).nil?
+  end
+end
+
+File.open(File.join(__dir__, 'input.txt')) do |file|
+  changes = file.each.map(&:to_i)
+  puts "Resulting frequency: #{changes.sum}"
+  puts "First duplicate frequency: #{first_duplicate_frequency(changes)}"
+end
